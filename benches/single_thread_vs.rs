4320263a7fe7aae507aefcafbaa685bc168316a5
ocornoc <ocornoc@protonmail.com>
2021-04-21 16:47:18 -0700

    change benches, add weaken bench

--- benches/single_thread_vs.rs
+++ benches/single_thread_vs.rs
@@ -79,21 +79,21 @@ fn clone_bench(c: &mut Criterion) {
         .collect::<Vec<_>>();
     group.bench_with_input("Rc", &rcs, |b, rcs| {
         b.iter_batched(
-            || black_box(rcs.clone()),
+            || rcs,
             |vs| black_box(vs.iter().map(Rc::clone).collect::<Vec<_>>()),
             BatchSize::NumIterations(1000),
         )
     });
     group.bench_with_input("Arc", &arcs, |b, arcs| {
         b.iter_batched(
-            || black_box(arcs.clone()),
+            || arcs,
             |vs| black_box(vs.iter().map(Arc::clone).collect::<Vec<_>>()),
             BatchSize::NumIterations(1000),
         )
     });
     group.bench_with_input("Tark", &tarks, |b, tarks| {
         b.iter_batched(
-            || black_box(tarks.clone()),
+            || tarks,
             |vs| black_box(vs.iter().map(Tark::clone).collect::<Vec<_>>()),
             BatchSize::NumIterations(1000),
         )
@@ -128,10 +128,71 @@ fn deref_bench(c: &mut Criterion) {
     black_box(tark);
 }
 
-criterion_group! {
-    name = new;
-    config = Criterion::default();
-    targets = new_bench, clone_bench, deref_bench,
+fn weaken_bench(c: &mut Criterion) {
+    const AMORTIZED: u64 = 1000;
+    let mut group = c.benchmark_group("[Rc/Arc/Tark]::downgrade(), once");
+    group.throughput(Throughput::Elements(1));
+    let rc = Rc::new(0);
+    let arc = Arc::new(0);
+    let tark = Tark::new(0);
+    group.bench_with_input("Rc", &rc, |b, rc| {
+        b.iter_with_large_drop(|| black_box(Rc::downgrade(black_box(rc))));
+    });
+    group.bench_with_input("Arc", &arc, |b, arc| {
+        b.iter_with_large_drop(|| black_box(Arc::downgrade(black_box(arc))));
+    });
+    group.bench_with_input("Tark", &tark, |b, tark| {
+        b.iter_with_large_drop(|| black_box(Tark::downgrade(black_box(tark))));
+    });
+    group.finish();
+    let mut group = c.benchmark_group("[Rc/Arc/Tark]::downgrade(), amortized");
+    group.throughput(Throughput::Elements(AMORTIZED));
+    let rcs = std::iter::repeat(rc.clone())
+        .take(AMORTIZED as usize)
+        .into_iter()
+        .collect::<Vec<_>>();
+    let arcs = std::iter::repeat(arc.clone())
+        .take(AMORTIZED as usize)
+        .into_iter()
+        .collect::<Vec<_>>();
+    let tarks = std::iter::repeat(tark.clone())
+        .take(AMORTIZED as usize)
+        .into_iter()
+        .collect::<Vec<_>>();
+    group.bench_with_input("Rc", &rcs, |b, rcs| {
+        b.iter_batched(
+            || rcs,
+            |vs| black_box(vs.iter().map(Rc::downgrade).collect::<Vec<_>>()),
+            BatchSize::NumIterations(1000),
+        )
+    });
+    group.bench_with_input("Arc", &arcs, |b, arcs| {
+        b.iter_batched(
+            || arcs,
+            |vs| black_box(vs.iter().map(Arc::downgrade).collect::<Vec<_>>()),
+            BatchSize::NumIterations(1000),
+        )
+    });
+    group.bench_with_input("Tark", &tarks, |b, tarks| {
+        b.iter_batched(
+            || tarks,
+            |vs| black_box(vs.iter().map(Tark::downgrade).collect::<Vec<_>>()),
+            BatchSize::NumIterations(1000),
+        )
+    });
+    group.finish();
+    black_box(rc);
+    black_box(arc);
+    black_box(tark);
+    black_box(rcs);
+    black_box(arcs);
+    black_box(tarks);
 }
 
-criterion_main!(new);
+criterion_group! {
+    name = benches;
+    config = Criterion::default();
+    targets = new_bench, clone_bench, deref_bench, weaken_bench,
+}
+
+criterion_main!(benches);
